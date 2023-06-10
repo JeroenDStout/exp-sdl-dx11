a518ef675fd31096c7ffc1f6abf74b8953b4af59
JeroenDStout <jeroenstout@gmail.com>
2023-06-10 16:17:32 +0200

    Add main loop

--- src/experiment/experiment.cpp
+++ src/experiment/experiment.cpp
@@ -8,6 +8,10 @@
 namespace experiment {
 
 void run_experiment() {
+    //
+    // --- Init
+    //
+
 	std::cout << "Starting experiment" << std::endl;
 
     SDL_Init(SDL_INIT_VIDEO);
@@ -25,8 +29,29 @@ void run_experiment() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
-    
-    SDL_Delay(3000);
+
+    //
+    // --- Main loop
+    //
+
+	std::cout << "Main loop" << std::endl;
+
+    bool      running = true;
+    SDL_Event sdl_event;
+
+    while (running && SDL_WaitEvent(&sdl_event)) {
+        switch (sdl_event.type) {
+          default: break;
+          case SDL_EventType::SDL_QUIT:
+	        std::cout << "Quit" << std::endl;
+            running = false;
+			break;
+        }
+    }
+
+    //
+    // --- Cleanup
+    //
 
 	std::cout << "Destroying window" << std::endl;
     
