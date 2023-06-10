21734296c2ce8e29f26813d5f3a11f61ed1ce730
JeroenDStout <jeroenstout@gmail.com>
2023-06-10 16:31:55 +0200

    Add random background colour upon click

--- src/experiment/experiment.cpp
+++ src/experiment/experiment.cpp
@@ -2,6 +2,7 @@
 
 #include "SDL.h"
 
+#include <random>
 #include <iostream>
 
 
@@ -36,15 +37,37 @@ void run_experiment() {
 
 	std::cout << "Main loop" << std::endl;
 
+    std::random_device random_dev;
+    std::mt19937       random_gen(random_dev());
+    std::uniform_int_distribution<std::mt19937::result_type> random_byte(0x00, 0xFF);
+
     bool      running = true;
     SDL_Event sdl_event;
 
     while (running && SDL_WaitEvent(&sdl_event)) {
         switch (sdl_event.type) {
-          default: break;
+          default:
+            break;
+
           case SDL_EventType::SDL_QUIT:
 	        std::cout << "Quit" << std::endl;
             running = false;
+			break;
+
+          case SDL_EventType::SDL_MOUSEBUTTONDOWN:
+	        std::cout << "Button press" << std::endl;
+
+            // Set a random background colour
+            SDL_SetRenderDrawColor(
+              renderer,
+              (std::uint8_t)random_byte(random_gen),
+              (std::uint8_t)random_byte(random_gen),
+              (std::uint8_t)random_byte(random_gen),
+              SDL_ALPHA_OPAQUE
+            );
+            SDL_RenderClear(renderer);
+            SDL_RenderPresent(renderer);
+
 			break;
         }
     }
